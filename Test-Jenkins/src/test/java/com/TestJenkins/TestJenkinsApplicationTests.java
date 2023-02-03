67632575a0c968eca652e7d11385cecf0326aed1
이정주 <wjdwn3132@naver.com>
2023-02-03 11:33:21 +0900

    asdf

---
+++ Test-Jenkins/src/test/java/com/TestJenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
